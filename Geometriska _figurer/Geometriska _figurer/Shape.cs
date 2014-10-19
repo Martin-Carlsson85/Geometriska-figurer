b7e958e2c1dc539a429c273419368384ffb73dcd
Martin Carlsson <mc22dg@student.lnu.se>
2014-10-19 13:55:58 +0200

    Nästan klar med klassen Shape

--- Geometriska _figurer/Geometriska _figurer/Shape.cs
+++ Geometriska _figurer/Geometriska _figurer/Shape.cs
@@ -7,5 +7,63 @@ namespace Geometriska__figurer
 {
     public class Shape : Program
     {
+        //Fält
+        private double _length;
+        private double _width;
+
+        //Egenkaper
+        public abstract double Area { get; }
+
+
+        public double Length
+        {
+            get { return _length; }
+
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException();
+
+                }
+
+                _length = value;
+
+            }
+        }
+
+        public double Perimeter { get; }
+
+        public double Width
+        {
+            get { return _width; }
+
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException();
+
+                }
+
+                _width = value;
+
+            }
+        }
+
+        //Konstruktor
+        protected Shape(double length, double width)
+        {
+              Length = _length;
+              Width = _width;
+
+        }
+
+        public override string ToString()
+        {
+
+           
+
+        }
     }
 }
