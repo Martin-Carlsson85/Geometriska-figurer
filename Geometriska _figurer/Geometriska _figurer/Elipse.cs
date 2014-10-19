6828b52851365d7b64036169f2c0a2fd798a6d57
Martin Carlsson <mc22dg@student.lnu.se>
2014-10-19 14:27:22 +0200

    Mer eller mindre klar med Rectangle och Elipse

--- Geometriska _figurer/Geometriska _figurer/Elipse.cs
+++ Geometriska _figurer/Geometriska _figurer/Elipse.cs
@@ -7,5 +7,22 @@ namespace Geometriska__figurer
 {
     public class Elipse : Shape
     {
+        //Egenskap
+        public override double Area        {
+            get { return Math.PI * (Length / 2) * (Width / 2); }
+        }
+
+        //Egenskap
+        public override double Permitter
+        {
+            get { return (Math.PI * Math.Sqrt(2 * (Length / 2) * (Length / 2) + 2 * (Width / 2) * (Width / 2))); }
+        }
+
+        //Konstruktor
+        public Elipse(double length, double width)
+            : base(length, width)
+        {
+
+        }
     }
 }
--- Geometriska _figurer/Geometriska _figurer/Rectangle.cs
+++ Geometriska _figurer/Geometriska _figurer/Rectangle.cs
@@ -7,5 +7,24 @@ namespace Geometriska__figurer
 {
     public class Rectangle : Shape
     {
+        //Egenskap
+        public override double Area
+        {
+            get { return Length * Width; }
+
+        }
+
+        //Egenskap
+        public override double Perimiter
+        {
+            get { return 2 * Length + 2 * Width; }
+        }
+
+        //Konstruktor
+        public Rectangle(double length, double width)
+            : base(length, width)
+        {
+
+        }
     }
 }
